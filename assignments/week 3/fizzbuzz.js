9a371901bc5b8efcbc24632b071c7dd8e65d9856
nishadbanur9 <nbanur@hawk.iit.edu>
2017-09-13 14:14:28 -0500

    Add files via upload

--- assignments/week 3/fizzbuzz.js
+++ assignments/week 3/fizzbuzz.js
@@ -1,1 +1,26 @@
+var num1 = prompt("First number");
+var num2 = prompt("Second number");
 
+var i = parseInt(num1);
+var j = parseInt(num2);
+
+fizzbuzz(i,j);
+
+function fizzbuzz(i,j){
+if(i>j){
+  window.alert("Please enter the range correctly");
+}
+else
+{
+  for (var a=i; a<=j; a++)
+  {
+    if ( a % 3 == 0 && a%5==0)
+        console.log("fizzbuzz");
+    else if ( a%3 == 0)
+        console.log("fizz");
+    else if ( a%5 == 0)
+        console.log("buzz");
+    else
+        console.log(a);
+  }
+}}
